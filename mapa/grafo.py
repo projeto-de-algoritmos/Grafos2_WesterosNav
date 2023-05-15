1187c2bc920a37bdeb4c842d10e77cb26da3c522
victorleaoo <victor.pessoal1203@gmail.com>
2023-05-14 22:00:58 -0300

    mapeamento do mapa em json para grafo

--- mapa/grafo.py
+++ mapa/grafo.py
@@ -24,3 +24,26 @@ class Grafo:
             print(f"Vertice {vertice}:")
             for vizinho in lista_adjacencia:
                 print(f"\t Vizinho: {vizinho['vizinho']}, Distancia: {vizinho['distancia']}")
+
+# Le o arquivo JSON
+with open('./mapa.json', 'r') as f:
+    data = json.load(f)
+
+# Cria o grafo e o dicionario de mapeamento
+grafo = Grafo()
+mapeamento = {}
+
+# Adiciona as cidades como vertices e armazena o mapeamento
+for cidade in data['cities']:
+    grafo.adiciona_vertice(cidade['id'])
+    mapeamento[cidade['id']] = cidade['name']
+
+# Adiciona as fronteiras como arestas
+for fronteira in data['borders']:
+    cidade1, cidade2 = fronteira['cidades']
+    distancia = fronteira['distancia']
+    grafo.adiciona_aresta(cidade1, cidade2, distancia)
+
+# Imprime o grafo e o mapeamento
+grafo.imprime_grafo()
+print(mapeamento)
